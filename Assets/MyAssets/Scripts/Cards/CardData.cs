c084383aaec4eef0a58366b4d557471a723d8729
AlebiGit <borg.ale01@gmail.com>
2023-10-20 14:31:57 +0200

    Update CardData.cs

--- Assets/MyAssets/Scripts/Cards/CardData.cs
+++ Assets/MyAssets/Scripts/Cards/CardData.cs
@@ -6,12 +6,14 @@ using UnityEngine;
 public class CardData : ScriptableObject
 {
     [SerializeField] private Sprite sprite;
+    [SerializeField] private Sprite spriteLarge;
     [SerializeField] private string cardName = "Default Name";
     [SerializeField] private string cardDescription = "Default Description";
     [SerializeField] private CardAudioProfile cardAudioProfile;
     [SerializeField] private SkillData[] skills = new SkillData[3];
 
     public Sprite Sprite => sprite;
+    public Sprite SpriteLarge => spriteLarge;
     public string CardName => cardName;
     public string CardDescription => cardDescription;
     public SkillData[] Skills => skills;
