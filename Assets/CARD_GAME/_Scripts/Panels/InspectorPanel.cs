60034a6ba765d0786ae793fd4707b69f1a900f0c
AlebiGit <borg.ale01@gmail.com>
2023-11-10 15:37:49 +0100

    Fix vari

--- Assets/CARD_GAME/_Scripts/Panels/InspectorPanel.cs
+++ Assets/CARD_GAME/_Scripts/Panels/InspectorPanel.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class InspectorPanel : MonoBehaviour, IPointerClickHandler
 {
-   
-    [SerializeField]Card card;
+    [SerializeField] private Card card;
+    [SerializeField] private TMP_Text description;
 
     public void OnPointerClick(PointerEventData eventData) {
         gameObject.SetActive(false);
@@ -14,6 +15,6 @@ public class InspectorPanel : MonoBehaviour, IPointerClickHandler
 
     public void SetCardToBeInspected(Card _card) {
         card.LoadData(_card.CardData);
-        
+        description.text = _card.CardData.CardDescription;
     }
 }
