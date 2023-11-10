6f9daecba6b65a6e9a570ea6cab1ffd816abe08b
AlebiGit <borg.ale01@gmail.com>
2023-11-10 10:10:41 +0100

    Menù impostazioni rifatto

--- Assets/CARD_GAME/_Scripts/SettingsButton.cs
+++ Assets/CARD_GAME/_Scripts/SettingsButton.cs
@@ -4,18 +4,36 @@ using UnityEngine;
 
 public class SettingsButton : MonoBehaviour
 {
-    [SerializeField] GameObject pausePanel;
+    [SerializeField] private RectTransform pausePanel;
+    [SerializeField] private Vector3 offScreenPosition;
+    [SerializeField] private Vector3 inScreenPosition;
+    [SerializeField] private float speed;
+    private bool isShowing = false;
 
     public void TogglePausePanelOnAndOff()
     {
-        if (!pausePanel.activeInHierarchy) 
+        /*if (!pausePanel.activeInHierarchy) 
         {
             pausePanel.SetActive(true);
         }
         else
         {
             pausePanel.SetActive(false);
-        }
-             
+        }*/
+        StopAllCoroutines();
+        StartCoroutine(SlideMovementCoroutine());
     }
+
+    private IEnumerator SlideMovementCoroutine()
+    {
+        isShowing = !isShowing;
+        Vector2 targetPos = isShowing ? new Vector2(-pausePanel.sizeDelta.x, 0) : Vector2.zero;
+        while (pausePanel.anchoredPosition != targetPos)
+        {
+            Debug.Log("Moving!");
+            pausePanel.anchoredPosition = Vector2.Lerp(pausePanel.anchoredPosition, targetPos, speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
 }
